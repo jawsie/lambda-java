d7e595e7d801d3c05cc8bdef80d6cc8047667515
jawahar antao <jawahar.antao@outlook.com>
2025-08-16 16:59:37 -0500

    bye

--- src/main/java/com/example/MyLambdaHandler.java
+++ src/main/java/com/example/MyLambdaHandler.java
@@ -8,7 +8,7 @@ public class MyLambdaHandler  implements RequestHandler<String, String> {
 
     @Override
     public String handleRequest(String input, Context context) {
-        return "Hello from Lambda! Input: " + input;
+        return "Bye from Lambda! Input: " + input;
     }
 
 }
