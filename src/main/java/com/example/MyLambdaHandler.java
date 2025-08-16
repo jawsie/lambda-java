1367b7ed8f898932192dec2b4e5d70e7a7da04a1
jawahar antao <jawahar.antao@outlook.com>
2025-08-16 15:40:03 -0500

    working version of lambda

---
+++ src/main/java/com/example/MyLambdaHandler.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+
+public class MyLambdaHandler  implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        return "Hello from Lambda! Input: " + input;
+    }
+
+}
